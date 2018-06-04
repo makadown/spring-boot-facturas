4f173081b1d2a79f99bc14ecfa379f474ad96b2d
Mario Serrano <maseflores79@gmail.com>
2018-06-04 15:35:50 -0700

    Finalizado importacion a PDF con todo e Internacionalización

--- src/main/java/com/makadown/springboot/app/auth/handler/LoginSuccessHandler.java
+++ src/main/java/com/makadown/springboot/app/auth/handler/LoginSuccessHandler.java
@@ -1,19 +1,29 @@
 package com.makadown.springboot.app.auth.handler;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 @Component
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Autowired
+	private LocaleResolver localeResolver;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -23,11 +33,14 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 			SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
 
 			FlashMap flashMap = new FlashMap();
-
-			flashMap.put("success", "Bienvenid@ " + authentication.getName() + "!, has iniciado sesión con éxito");
+			
+			Locale locale = localeResolver.resolveLocale(request);
+			String mensaje = String.format(messageSource.getMessage("text.login.success",null, locale), authentication.getName());
+			
+			flashMap.put("success", mensaje);
 			flashMapManager.saveOutputFlashMap(flashMap, request, response);
 
-			logger.info("El usuario " + authentication.getName() + " ha iniciado sesión con éxito");
+			//logger.info("El usuario " + authentication.getName() + " ha iniciado sesión con éxito");
 		}
 
 		super.onAuthenticationSuccess(request, response, authentication);
